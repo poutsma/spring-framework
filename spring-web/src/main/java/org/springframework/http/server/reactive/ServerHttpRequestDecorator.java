b1d841b204ff45284f40652ef2792267b1614a96
Arjen Poutsma <arjen.poutsma@broadcom.com>
2024-05-29 11:12:04 +0200

    Polishing

--- spring-web/src/main/java/org/springframework/http/server/reactive/ServerHttpRequestDecorator.java
+++ spring-web/src/main/java/org/springframework/http/server/reactive/ServerHttpRequestDecorator.java
@@ -115,12 +115,6 @@ public class ServerHttpRequestDecorator implements ServerHttpRequest {
 	}
 
 	@Override
-	@Nullable
-	public <T> T getNativeRequest() {
-		return this.delegate.getNativeRequest();
-	}
-
-	@Override
 	public Flux<DataBuffer> getBody() {
 		return getDelegate().getBody();
 	}
@@ -135,13 +129,16 @@ public class ServerHttpRequestDecorator implements ServerHttpRequest {
 	 * @since 5.3.3
 	 */
 	public static <T> T getNativeRequest(ServerHttpRequest request) {
-		T nativeRequest = request.getNativeRequest();
-		if (nativeRequest == null) {
+		if (request instanceof AbstractServerHttpRequest abstractServerHttpRequest) {
+			return abstractServerHttpRequest.getNativeRequest();
+		}
+		else if (request instanceof ServerHttpRequestDecorator serverHttpRequestDecorator) {
+			return getNativeRequest(serverHttpRequestDecorator.getDelegate());
+		}
+		else {
 			throw new IllegalArgumentException(
 					"Can't find native request in " + request.getClass().getName());
 		}
-
-		return nativeRequest;
 	}
 
 
